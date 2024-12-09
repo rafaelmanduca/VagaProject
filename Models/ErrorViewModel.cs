1c49a6e460361a3181ca50b04b660d539511ac3d
unknown <rafael.costamanduca@gmail.com>
2024-12-09 19:01:01 -0300

    feat Beginning of the project with the vacant class

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vaga.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Vaga.cs
@@ -0,0 +1,43 @@
+﻿namespace Estacionamento.Models
+{
+    public class Vaga
+    {
+        public int Id { get; set; }
+        
+        public int NumeroVaga { get; set; }
+  
+        public string Tipo { get; set; }
+
+        public Boolean Status { get; set; }
+
+        public DateTime Ocupacao { get; set; }
+
+        public DateTime Liberacao { get; set; }
+
+
+
+        public Vaga()
+        {
+
+        }
+
+        public Vaga(int id, int numeroVaga, string tipo, bool status, DateTime ocupacao, DateTime liberacao)
+        {
+            Id = id;
+            NumeroVaga = numeroVaga;
+            Tipo = tipo;
+            Status = status;
+            Ocupacao = ocupacao;
+            Liberacao = liberacao;
+        }
+
+       
+
+    }
+
+
+
+   
+  
+
+}
